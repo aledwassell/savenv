c274f343869e9c4158cb661a9d8b2b080583a7f4
aledwassell <aledwassell@googlemail.com>
2018-08-18 12:27:09 +0100

    adding mongoose backend

--- server/server.js
+++ server/server.js
@@ -1,76 +1,41 @@
 const express = require('express'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
+    MongoClient = require('mongodb'),
+    assert = require('assert'),
     PORT = 4000,
     app = express(),
     router = express.Router();
 
-let Issue = require('./models/Issue');
+let Dat = require('./models/dat');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:4000/issues', { useNewUrlParser: true });
+const url = 'mongodb://localhost:27017/';
 
-const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log('MongoDB database connection established!')
+router.route('/api/times').get((req, res) => {
+    res.send('this is the get route');
 });
-
-router.route('/issues/add').post((req, res) => {
-    let issue = new Issue(req.body);
-    issue.save()
-        .then(issue => {
-            res.status(200).json({'issue': 'Added successfully'});
+router.route('/api').post((req, res) => {
+    let dat = new Dat(req.body);
+    console.log(dat);
+    dat.save()
+        .then(d => {
+            console.log(d);
+            res.status(200).json({'d': 'Added successfully'});
         })
         .catch(err => {
             res.status(400).send('Failed to create new record');
         });
 });
-router.route('/issues').get((req, res) => {
-    Issue.find((err, issues) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issues);
-    });
-});
-router.route('/issues/:id').get((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issue);
-    })
-});
-router.route('/issues/update/:id').post((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (!issue)
-            return next(new Error('Could not load Document'));
-        else {
-            issue.title = req.body.title;
-            issue.responsible = req.body.responsible;
-            issue.description = req.body.description;
-            issue.severity = req.body.severity;
-            issue.status = req.body.status;
-            issue.save().then(issue => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
-});
-router.route('/issues/delete/:id').get((req, res) => {
-    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
-        if (err)
-            res.json(err);
-        else
-            res.json('Removed successfully');
-    });
-});
+
+MongoClient.connect(url, (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected to server successfully');
+    db.close();
+})
 
 app.use('/', router);
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
